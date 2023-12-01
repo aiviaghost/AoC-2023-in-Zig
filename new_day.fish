24db27123df0100b5bb9a560d35babb0c1a4400c
aiviaghost <smurfplatta@gmail.com>
2023-12-01 14:34:58 +0100

    Some setup

---
+++ new_day.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+
+set day (math (count (ls -d day-*)) + 1)
+
+mkdir "day-$day"
+cd "day-$day"
+
+curl --cookie= "session=$(cat ../.secret-cookie)" https://adventofcode.com/2023/day/$day/input > input.txt
+
+cp ../template.zig ./main.zig
