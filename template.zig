f7a7500a16b25cd82404f558bba536045966c1a2
aiviaghost <smurfplatta@gmail.com>
2023-12-02 23:15:45 +0100

    Update template

--- template.zig
+++ template.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
+const input = @embedFile("input.txt");
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+fn solve_1() !void {
+    var lines = std.mem.tokenizeSequence(u8, input, "\n");
+    _ = lines;
+}
+
+fn solve_2() !void {
+    var lines = std.mem.tokenizeSequence(u8, input, "\n");
+    _ = lines;
+}
+
 pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
+    try solve_1();
+    try solve_2();
 }
