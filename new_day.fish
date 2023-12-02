6714fbe80cba237c65565d699300c5048e2f2221
aiviaghost <smurfplatta@gmail.com>
2023-12-02 23:30:20 +0100

    Fix new_day-script

--- new_day.fish
+++ new_day.fish
@@ -2,9 +2,9 @@
 
 set day (math (count (ls -d day-*)) + 1)
 
-mkdir "day-$day"
-cd "day-$day"
+mkdir day-$day
+cd day-$day
 
-curl --cookie= "session=$(cat ../.secret-cookie)" https://adventofcode.com/2023/day/$day/input > input.txt
+curl --cookie session=(cat ../.secret-cookie) https://adventofcode.com/2023/day/$day/input > input.txt
 
 cp ../template.zig ./main.zig
